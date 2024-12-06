01e0b758fc2cf002726eebb6096b96f51e49bfbb
shiri <irihsmoc@gmail.com>
2024-12-07 02:08:59 +0900

    update: 追加してみる

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   images: {
-    domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
+    domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com', 'lh3.googleusercontent.com'],
   },
 }
