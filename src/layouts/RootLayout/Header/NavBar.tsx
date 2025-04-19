a5d90be1167d6b1092d2416be1087988e73b1eb6
shiri <irihsmoc@gmail.com>
2025-04-19 23:47:41 +0900

    add: portfolio

--- src/layouts/RootLayout/Header/NavBar.tsx
+++ src/layouts/RootLayout/Header/NavBar.tsx
@@ -2,7 +2,7 @@ import styled from "@emotion/styled"
 import Link from "next/link"
 
 const NavBar: React.FC = () => {
-  const links = [{ id: 1, name: "About", to: "/about" }]
+  const links = [{ id: 1, name: "About", to: "/about" }, { id: 2, name: "Portfolio", to: "/portfolio" }]
   return (
     <StyledWrapper className="">
       <ul>
