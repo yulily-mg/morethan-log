6cf75efe2a5e599b5c3d714591df49f980752a84
shiri <irihsmoc@gmail.com>
2025-08-10 22:59:19 +0900

    add: illust link

--- src/layouts/RootLayout/Header/NavBar.tsx
+++ src/layouts/RootLayout/Header/NavBar.tsx
@@ -2,7 +2,7 @@ import styled from "@emotion/styled"
 import Link from "next/link"
 
 const NavBar: React.FC = () => {
-  const links = [{ id: 1, name: "About", to: "/about" }, { id: 2, name: "Portfolio", to: "/portfolio" }]
+  const links = [{ id: 1, name: "About", to: "/about" }, { id: 2, name: "Portfolio", to: "/portfolio" }, { id: 3, name: "Illustration", to: "/illustration" }]
   return (
     <StyledWrapper className="">
       <ul>
